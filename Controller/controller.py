0a35b29d62695171693ff20d09ba561ea6db823d
TheBiggerFish <cameron.haddock@live.longwood.edu>
2019-03-21 18:45:49 -0400

    A flask server to test functionality for controller communicating with other programs

--- Controller/controller.py
+++ Controller/controller.py
@@ -1,22 +1,10 @@
-import os
+from flask import Flask
 
-def child():
-    print('\n new child ', os.getpid())
-    os._exit(0)
+app = Flask(__name__)
 
-def parent():
-    parentread,childwrite = os.pipe()
-    parentwrite,childread = os.pipe()
-    pid = os.fork()
-    if pid == 0:
-        child()
-    else:
-        print('parent ', os.getpid())
-        print('child ', pid)
-        os._exit(0)
+@app.route("/")
+def hello_world():
+    return "Hello World"
 
-parent()
-
-#https://www.python-course.eu/pipes.php
-#https://www.python-course.eu/forking.php
-#https://www.google.com/search?client=firefox-b-1-d&q=set+up+socket+between+python+and+c%2B%2B
+if __name__ == "__main__":
+    app.run()
