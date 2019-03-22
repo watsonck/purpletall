0d9f4bdba3d7f781577fb95370d2795f63d8cebe
whitebryan <48658686+whitebryan@users.noreply.github.com>
2019-03-21 21:14:07 -0400

    Testing of connecting the controller to the view

--- Controller/controller.py
+++ Controller/controller.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World"
 
+@app.route("/TODO")
+def todo_test():
+    return "TODO this is from the flask server"
+
+@app.route("/INPR")
+def inpr_test():
+    return "INPR 0"
+
+@app.route("/COMP")
+def comp_test():
+    return "COMP 0"
+
 if __name__ == "__main__":
     app.run()
--- View/Terminal/curses_console.py
+++ View/Terminal/curses_console.py
@@ -1,6 +1,6 @@
 import curses
 import time
-
+import requests
 
 win_list = []
 screen = -1
@@ -64,33 +64,69 @@ def kanban():
     screen.addstr(1,int((split/2)*3)-5, "IN PROGRESS", curses.A_REVERSE)
     screen.addstr(1,int((split/2)*5)-5, "COMPLETE", curses.A_REVERSE)
 
+    server_test = 0
     while True:
         global tasks
         global in_prog
         global complete
 
-        str1 = get_text(split-2)
-        parsed = parse_cmd(str1)
-
-        if parsed[0].decode() == "QUIT":
+        #str1 = get_text(split-2)
+        str1 = ""
+        if server_test == 3:
             break
-        elif parsed[0].decode() == "TODO":
+        elif server_test == 0:
+            str1 = requests.get("http://127.0.0.1:5000/TODO").text
+        elif server_test == 1:
+            str1 = requests.get("http://127.0.0.1:5000/INPR").text
+        elif server_test == 2:
+            str1 = requests.get("http://127.0.0.1:5000/COMP").text
+        parsed = parse_cmd(str1)
+        print(parsed)
+        server_test = server_test + 1
+        
+
+        #For when typing in input
+        # if parsed[0].decode() == "QUIT":
+        #     break
+        # elif parsed[0].decode() == "TODO":
+        #     task = ""
+        #     for word in parsed[1:]:
+        #         task = task + word.decode() + " "
+        #     tasks.append(task)
+        # elif parsed[0].decode() == "INPR":
+        #     if int(parsed[1].decode()) <= len(tasks)-1 :
+        #         in_prog.append(tasks[int(parsed[1].decode())])
+        #         tasks.pop(int(parsed[1].decode()))
+        #         for x in range(2, split-2):
+        #             screen.addstr(2+(int(parsed[1].decode()*2)), x, " ")
+        # elif parsed[0].decode() == "COMP":
+        #     if int(parsed[1].decode()) <= len(in_prog)-1:
+        #         complete.append(in_prog[int(parsed[1].decode())])
+        #         in_prog.pop(int(parsed[1].decode()))
+        #         for x in range(split+2, split+split-2):
+        #             screen.addstr(2+(int(parsed[1].decode()*2)), x, " ")
+        
+        
+        #for when receiving from server
+        if parsed[0] == "QUIT":
+            break
+        elif parsed[0] == "TODO":
             task = ""
             for word in parsed[1:]:
-                task = task + word.decode() + " "
+                task = task + word + " "
             tasks.append(task)
-        elif parsed[0].decode() == "INPR":
-            if int(parsed[1].decode()) <= len(tasks)-1 :
-                in_prog.append(tasks[int(parsed[1].decode())])
-                tasks.pop(int(parsed[1].decode()))
+        elif parsed[0] == "INPR":
+            if int(parsed[1]) <= len(tasks)-1 :
+                in_prog.append(tasks[int(parsed[1])])
+                tasks.pop(int(parsed[1]))
                 for x in range(2, split-2):
-                    screen.addstr(2+(int(parsed[1].decode()*2)), x, " ")
-        elif parsed[0].decode() == "COMP":
-            if int(parsed[1].decode()) <= len(in_prog)-1:
-                complete.append(in_prog[int(parsed[1].decode())])
-                in_prog.pop(int(parsed[1].decode()))
+                    screen.addstr(2+(int(parsed[1]*2)), x, " ")
+        elif parsed[0] == "COMP":
+            if int(parsed[1]) <= len(in_prog)-1:
+                complete.append(in_prog[int(parsed[1])])
+                in_prog.pop(int(parsed[1]))
                 for x in range(split+2, split+split-2):
-                    screen.addstr(2+(int(parsed[1].decode()*2)), x, " ")
+                    screen.addstr(2+(int(parsed[1]*2)), x, " ")
 
  
         for i in range(len(tasks)):
