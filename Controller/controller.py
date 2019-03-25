f7a3ec4737b3c2c7d182f29f125e516d742cd117
TheBiggerFish <clhnumber4@gmail.com>
2019-03-25 21:30:13 +0000

    Modified controller.py to include function for add and move tasks, as well as modify run command to open up flask server to outside access

--- Controller/controller.py
+++ Controller/controller.py
@@ -5,15 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World"
+	return "Hello World"
 
 @app.route("/TASK/<string:task>")
 def new_task(task):
-    n_task = ""
-    for word in task.split():
-        n_task = n_task + word + " "
-    return n_task
+	n_task = ""
+	for word in task.split():
+		n_task = n_task + word + " "
+	return n_task
 
+@app.route("/add/<string:task_name>/<string:desc>/<string:ect>")
+def add(task_name,desc,ect):
+	return task_name + "  " + desc + "  " + ect
+
+@app.route("/move/<string:task_id>/<string:dest>")
+def move(task_id,dest):
+	return task_id + "  " + desc
 
 if __name__ == "__main__":
-    app.run()
+	app.run(host='0.0.0.0')
