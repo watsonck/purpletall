ec73fa59e5ffa1291d6765c21d315e09e0d11c4a
Michael Montgomery <montgmi@bullroarer.cs.longwood.edu>
2019-03-27 01:09:09 -0400

    filled in baseline remove function in controler shortened a foreign key assigning in database, cannot do other one same way as unsure of results todo: split GET and POST request handling in controler to decide correct output

--- Model/tasking.sql
+++ Model/tasking.sql
@@ -1,31 +1,26 @@
-CREATE TABLE Task  (id      	 SERIAL PRIMARY KEY,
-    		    name	 VARCHAR(20)
-		    description	 TEXT,
-		    stage	 VARCHAR(4),
-		    startTime    TEXT,
-		    exptCompTime TEXT,
-		    actCompTime  TEXT
+CREATE TABLE Task  (id		SERIAL PRIMARY KEY,
+    		    name			VARCHAR(20)
+		    description	TEXT,
+		    stage			VARCHAR(4),
+		    startTime		TEXT,
+		    exptCompTime	TEXT,
+		    actCompTime	TEXT
     );
 
-
-
-CREATE TABLE Users (userId  SERIAL PRIMARY KEY,
-  		    fname   VARCHAR(10),
-		    lname   VARCHAR(10),
-		    email   VARCHAR(30),
-		    gitname VARCHAR(20)
+CREATE TABLE Users (userId	SERIAL PRIMARY KEY,
+  		    fname			VARCHAR(10),
+		    lname			VARCHAR(10),
+		    email			VARCHAR(30),
+		    gitname		VARCHAR(20)
     );
 
-
-CREATE TABLE Logs  (taskId	 SERIAL,
-  		    contributor	 SERIAL,
-		    action	 TEXT,
-		    time	 TEXT,
-		    comments	 TEXT,
+CREATE TABLE Logs  (taskId	SERIAL,
+  		    contributor	SERIAL REFERENCES Users (userId),
+		    action		TEXT,
+		    time			TEXT,
+		    comments		TEXT,
 		    PRIMARY KEY(taskId, time)
     );
 
 ALTER TABLE Logs ADD FOREIGN KEY (taskId) REFRENCES Task(id);
-ALTER TABLE Logs ADD FOREIGN KEY (contributor) REFRENCES Users(userId);
-
 
