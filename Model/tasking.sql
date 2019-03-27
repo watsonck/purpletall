6bba284e028f7c73f4f659f172ac918be583a9ca
Colin Watson <watsonck@pippin.cs.longwood.edu>
2019-03-27 14:14:56 -0400

    testing my push capabilities

--- Model/tasking.sql
+++ Model/tasking.sql
@@ -1,24 +1,24 @@
-CREATE TABLE Task  (id		SERIAL PRIMARY KEY,
-    		    name			VARCHAR(20)
-		    description	TEXT,
-		    stage			VARCHAR(4),
+CREATE TABLE Task  (id			SERIAL PRIMARY KEY,
+    		    name		VARCHAR(20)
+		    description		TEXT,
+		    stage		VARCHAR(4),
 		    startTime		TEXT,
 		    exptCompTime	TEXT,
-		    actCompTime	TEXT
+		    actCompTime		TEXT
     );
 
 CREATE TABLE Users (userId	SERIAL PRIMARY KEY,
-  		    fname			VARCHAR(10),
-		    lname			VARCHAR(10),
-		    email			VARCHAR(30),
-		    gitname		VARCHAR(20)
+  		    fname	VARCHAR(10),
+		    lname	VARCHAR(10),
+		    email	VARCHAR(30),
+		    gitname	VARCHAR(20)
     );
 
 CREATE TABLE Logs  (taskId	SERIAL,
   		    contributor	SERIAL REFERENCES Users (userId),
-		    action		TEXT,
-		    time			TEXT,
-		    comments		TEXT,
+		    action	TEXT,
+		    time	TEXT,
+		    comments	TEXT,
 		    PRIMARY KEY(taskId, time)
     );
 
