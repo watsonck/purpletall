0154feed5225fcff535559b720f98d7b586ca926
root <root@purpletall.cs.longwood.edu>
2019-04-03 17:44:18 -0400

    Added git proof of concept

--- Controller/git_test.py
+++ Controller/git_test.py
@@ -1,7 +1,7 @@
 from git import Git
 
 path = '/home/purpletall/purpletall'
-datetime='2019-03-26 12:00:00'
+datetime='2019-03-30 12:00:00'
 
 g = Git(path) 
 loginfo = g.log('--since='+datetime,'--name-only')
