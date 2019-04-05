65551286833d69f897c10f97605f101f36025ccc
steelairship <shyheim.williams@live.longwood.edu>
2019-04-05 09:11:15 -0400

    edited unit test

--- Controller/unit_test_controller.py
+++ Controller/unit_test_controller.py
@@ -1,22 +1,38 @@
 import flask
 import unittest
-import controller
+from controller import app
 
 class Test(unittest.TestCase):
+
+    TEST_DB = 'test.db'
+
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
+        #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \ os.path.join(app.config['BASEDIR'], TEST_DB)
         self.app = app.test_client()
+        ##db.drop_all()
+        ##db.create_all()
 
     def tearDown(self):
         pass
 
-    def test_route(self):
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
 
-    def test_add(self):
 
-    def test_move(self):
+    #def test_add(self, task):
+
+    #def test_move(self):
+        #return self.app.post(
+            #'/move',
+
+    #def test_remove():
         
+        
+
 if __name__ == "__main__":
     unittest.main()
