43c1bea7ae37001d70197c850cfedcba2d85007f
whitebryan <48658686+whitebryan@users.noreply.github.com>
2019-04-04 21:12:45 -0400

    kanban print fixing

--- View/Terminal/curses_console.py
+++ View/Terminal/curses_console.py
@@ -104,7 +104,7 @@ def kanban_print(split, max_tasks, limit):
             if cur_tasks == max_tasks:
                 break
             else:
-                str1 = str(task) + ": " + boards[board][task]['name']
+                str1 = str(task) + ": " + boards[board][0]['name']
                 screen.addstr(2+(cur_tasks*2), 2+(split*cur_board), str1, curses.A_REVERSE)
                 screen.addstr(3+(cur_tasks*2), 3+(split*cur_board), boards[board][task]['user'], curses.A_REVERSE)
                 cur_tasks = cur_tasks + 1
