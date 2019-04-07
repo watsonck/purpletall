d4bbc5a4a9b8e0be0b478ee862c85cab6661a9c7
whitebryan <48658686+whitebryan@users.noreply.github.com>
2019-04-06 22:35:14 -0400

    tracker

--- View/Terminal/curses_console.py
+++ View/Terminal/curses_console.py
@@ -91,7 +91,7 @@ def send_recv(proj, cmd, args):
 def proc_resp(task):
     global boards
     test_str1 = 'B2: ' + str(len(boards)) + " " + str(len(boards['0'])) + " " + str(len(boards['1'])) + " " + str(len(boards['2']))
-    screen.addstr(70, 2, test_str1, curses.A_REVERSE)
+    screen.addstr(62, 2, test_str1, curses.A_REVERSE)
     for key1, board in boards.items():
         board.clear()
     test = 0
