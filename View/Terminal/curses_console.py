11f54ccc1d9c0ddac6625d580595cf23cf82cb22
whitebryan <48658686+whitebryan@users.noreply.github.com>
2019-04-08 00:17:20 -0400

    small syntax fix

--- View/Terminal/curses_console.py
+++ View/Terminal/curses_console.py
@@ -179,7 +179,7 @@ def kanban_print(split, max_tasks, limit, start = 0):
                     cur_board = last
                 str1 = ""
                 if task[2] == True: #Unicode emoji copied from https://www.compart.com/en/unicode/U+1F41B
-                    str1 = str(key2) + ": " + task[0] + " " + 
+                    str1 = str(key2) + ": " + task[0]
                     screen.addstr(2+(cur_tasks*2), 2+(split*cur_board), str1, curses.color_pair(1))
                     screen.addstr(3+(cur_tasks*2), 3+(split*cur_board), str(task[1]), curses.color_pair(1))
                 else:
