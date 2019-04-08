0104c37f6503afd040d1a836c626f2a2183e2d5f
whitebryan <48658686+whitebryan@users.noreply.github.com>
2019-04-07 23:12:04 -0400

    Testing info response

--- Controller/test_controller.py
+++ Controller/test_controller.py
@@ -1,11 +1,11 @@
 import requests,json
 
-print(requests.get('http://purpletall.cs.longwood.edu:5000/1/add?name={Bug1}&desc={This%20bug%20is%20in%20controller}&time={2019-05-1}&bug={true}').text)
+#print(requests.get('http://purpletall.cs.longwood.edu:5000/1/add?name={Bug1}&desc={This%20bug%20is%20in%20controller}&time={2019-05-1}&bug={true}').text)
 #json_string = requests.get('http://purpletall.cs.longwood.edu:5000/1/move?id=6&stage={DONE}').text
 #print(json_string);
 #obj = json.loads(json_string)
 #print(obj)
 #print(obj['metadata']['stages']['1'])
 #print(obj['stages']['0'][0]['name'])
-
+print(json.loads(requests.get('http://purpletall.cs.longwood.edu:5000/1/info?id=1'))
 #obj[stages][which stage][which task in stage][which item in task]
