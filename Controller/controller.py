33c597c36c7e01797b6bdfa63463345223462689
steelairship <shyheim.williams@live.longwood.edu>
2019-04-21 14:50:21 -0400

    edited list.html and projlist in controller.py

--- Controller/controller.py
+++ Controller/controller.py
@@ -261,7 +261,7 @@ def projlist():
 				'description': row['description']
 			})
                 if request.method is "POST":
-                    return render_template("list.html", project =data)
+                    return render_template("list.html", list  = data)
                 else:
                     return json.dumps(data) 
 	except:
