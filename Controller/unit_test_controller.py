82b2ab98217e29960f44c559310051bbc3cd95eb
Colin Watson <watsonck@lamport.cs.longwood.edu>
2019-04-24 18:46:27 -0400

    unittesting dawg4: this is getting old

--- Controller/unit_test_controller.py
+++ Controller/unit_test_controller.py
@@ -71,7 +71,7 @@ class Test(unittest.TestCase):
     def test_add(self, project):     
         @app.route("/<int:project>/add", methods=["GET", "POST"])
 #@app.route("http://purpletall.cs.longwood.edu:5000/1/add?name={unittest1}&desc={This%20is%20a%20unittest}&time={2019-05-1}&bug={true}")
-        add(1)
+        add(project)
         db = get_db()
         db.execute("SELECT name FROM Tasks WHERE name = 'unittest1' ")
         name = db.fetchone() 
