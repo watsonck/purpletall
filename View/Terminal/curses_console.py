92579fcb4c82c570db42ce1e03433fda4ed084ca
whitebryan <48658686+whitebryan@users.noreply.github.com>
2019-04-28 18:51:55 -0400

    debug output

--- View/Terminal/curses_console.py
+++ View/Terminal/curses_console.py
@@ -465,7 +465,9 @@ def proj_choice():
     while True:
         choice = get_text(splitx*3-2)
         parse = choice.decode().split()
-        screen.addstr(20,2,parse[1], curses.A_REVERSE)
+        screen.addstr(20,2,parse[0], curses.A_REVERSE)
+        if len(parse) >=2 :
+            screen.addstr(21,2,parse[1], curses.A_REVERSE)
         if choice.decode().upper() == 'QUIT':
             close_curses()
             exit()
