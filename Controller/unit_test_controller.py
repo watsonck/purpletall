f8ba89bb4e2caef3e9391e5740de3a1ac95074c6
Colin Watson <watsonck@vonneumann.cs.longwood.edu>
2019-04-28 17:04:57 -0400

    add code for move

--- Controller/unit_test_controller.py
+++ Controller/unit_test_controller.py
@@ -13,24 +13,24 @@ current_app.name
 
 class Test(unittest.TestCase):
 
-	with app.app_context():
-		print(current_app.name)
+    with app.app_context():
+        print(current_app.name)
 
 
 
-	def setUp(self):
-		app.config['TESTING'] = True
-		app.config['WTF_CSRF_ENABLED'] = False
-		app.config['DEBUG'] = False
-		self.app = app.test_client()
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
 
-	def tearDown(self):
-		pass
+    def tearDown(self):
+        pass
 
 #compare the connection's status code to 200, which is "ok"
-	def test_connection(self):
-		response = self.app.get('/', follow_redirects=True)
-		self.assertEqual(response.status_code, 200)
+    def test_connection(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 #test if homepage is displaying correctly
 #    def test_home(self):
@@ -38,60 +38,70 @@ class Test(unittest.TestCase):
 #            self.assertEqual(home(),render_template("/login.html", title = "Login"))
 
 #test if a user is in database
-	def test_user_in_db(self):
-		db = get_db()
-		db.execute("SELECT fname FROM Users WHERE fname = 'Colin' ")
-		fname = db.fetchone() 
-		db.execute("SELECT lname FROM Users WHERE lname = 'Watson' ")
-		lname = db.fetchone()
-		db.execute("SELECT email FROM Users WHERE email = 'colin.watson777@yahoo.com'")
-		email = db.fetchone()
-		db.execute("SELECT lab_user FROM Users WHERE lab_user = 'watsonck'")
-		gitname = db.fetchone() 
-		self.assertEqual(fname, {'fname': 'Colin'})
-		self.assertEqual(lname, {'lname': 'Watson'})
-		self.assertEqual(email, {'email': 'colin.watson777@yahoo.com'})
-		self.assertEqual(gitname, {'lab_user': 'watsonck'})
+    def test_user_in_db(self):
+        db = get_db()
+        db.execute("SELECT fname FROM Users WHERE fname = 'Colin' ")
+        fname = db.fetchone() 
+        db.execute("SELECT lname FROM Users WHERE lname = 'Watson' ")
+        lname = db.fetchone()
+        db.execute("SELECT email FROM Users WHERE email = 'colin.watson777@yahoo.com'")
+        email = db.fetchone()
+        db.execute("SELECT lab_user FROM Users WHERE lab_user = 'watsonck'")
+        gitname = db.fetchone() 
+        self.assertEqual(fname, {'fname': 'Colin'})
+        self.assertEqual(lname, {'lname': 'Watson'})
+        self.assertEqual(email, {'email': 'colin.watson777@yahoo.com'})
+        self.assertEqual(gitname, {'lab_user': 'watsonck'})
 
-	def test_project_in_db(self):
-		db = get_db()
-		db.execute("SELECT projId FROM Projects WHERE projId = 1")
-		projId = db.fetchone()
-		db.execute("SELECT name FROM Projects WHERE name = 'Testing Project'")
-		name = db.fetchone()
-		db.execute("SELECT description FROM Projects WHERE description = 'a project made to test program'")
-		description = db.fetchone()
-		self.assertEqual(projId, {'projid': 1})
-		self.assertEqual(name, {'name': 'Testing Project'})
-		self.assertEqual(description, {'description': 'a project made to test program'})
+    def test_project_in_db(self):
+        db = get_db()
+        db.execute("SELECT projId FROM Projects WHERE projId = 1")
+        projId = db.fetchone()
+        db.execute("SELECT name FROM Projects WHERE name = 'Testing Project'")
+        name = db.fetchone()
+        db.execute("SELECT description FROM Projects WHERE description = 'a project made to test program'")
+        description = db.fetchone()
+        self.assertEqual(projId, {'projid': 1})
+        self.assertEqual(name, {'name': 'Testing Project'})
+        self.assertEqual(description, {'description': 'a project made to test program'})
 
-	def test_add_and_del(self):
-		resp = requests.get("http://purpletall.cs.longwood.edu:5000/1/add?name={unittest1}&desc={This%20is%20a%20unittest}&time={2019-05-1}&bug={true}").text
-		self.assertNotEqual(json.loads(resp), "ERROR")
-		db = get_db()
-		db.execute("SELECT id FROM Task WHERE name = 'unittest1' AND description = 'This is a unittest' AND exptCompTime = '2019-05-1' AND bugged = True")
-		name = db.fetchone() 
-		resp = json.loads(resp)
-		taskid = -1
-		for key1, stage in resp['stages'].items():
-			for task in stage:
-				if task['name'] == 'unittest1' and task['is_bug'] == True:
-					taskid = task['id']
-		self.assertEqual(taskid, name['id'])
-		resp =  requests.get("http://purpletall.cs.longwood.edu:5000/1/remove?id="+str(taskid)).text
-		resp = json.loads(resp)
-		ids = []
-		for key1, stage in resp['stages'].items():
-				for task in stage:
-						ids.append(task['id'])
-		self.assertNotIn(taskid, ids)
+    def test_add_and_del(self):
+        resp = requests.get("http://purpletall.cs.longwood.edu:5000/1/add?name={unittest1}&desc={This%20is%20a%20unittest}&time={2019-05-1}&bug={true}").text
+        self.assertNotEqual(json.loads(resp), "ERROR")
+        db = get_db()
+        db.execute("SELECT id FROM Task WHERE name = 'unittest1' AND description = 'This is a unittest' AND exptCompTime = '2019-05-1' AND bugged = True")
+        name = db.fetchone() 
+        resp = json.loads(resp)
+        taskid = -1
+        for key1, stage in resp['stages'].items():
+            for task in stage:
+                if task['name'] == 'unittest1' and task['is_bug'] == True:
+                    taskid = task['id']
+        self.assertEqual(taskid, name['id'])
+        resp =  requests.get("http://purpletall.cs.longwood.edu:5000/1/remove?id="+str(taskid)).text
+        resp = json.loads(resp)
+        ids = []
+        for key1, stage in resp['stages'].items():
+            for task in stage:
+                    ids.append(task['id'])
+        self.assertNotIn(taskid, ids)
 
-        
+    def test_move(self): 
+        resp = requests.get("http://purpletall.cs.longwood.edu:5000/1/move?id=1?stage={complete}").text
+        self.assertNotEqual(json.loads(resp), "ERROR")
+        db = get_db()
+        db.execute("SELECT id FROM task WHERE projid = 1 AND stage = 'complete'")
+        result = db.fetchone()
+        resp = json.loads(resp)
+        stageName = none
+        for key1, stage in resp['stages'].items():
+            for task in stage:
+                if task['name'] == 'unittest1' and task['is_bug'] == True:
+                    taskid = task['id']
+        self.assertEqual(stage name, name['id'])
 
 
-    #def test_move(self):
-        #return self.app.post(
-            #'/move',
+
         
 if __name__ == "__main__":
 	unittest.main()
